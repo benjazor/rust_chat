86fc4ce0f3c28200e6218249e5e85d7729faf663
Benjazor <benjazor@gmail.com>
2021-08-23 15:43:22 +0200

    Add logs file

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
 use std::io::{ErrorKind, Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::path::Path;
 use std::sync::mpsc;
 use std::thread;
 
 const LOCAL: &str = "127.0.0.1:7070";
 const MSG_SIZE: usize = 32;
+const LOGS_PATH: &str = "logs.txt";
 
 fn sleep() {
     thread::sleep(::std::time::Duration::from_millis(100));
@@ -24,6 +27,8 @@ fn main() {
     println!("Starting the server at: {}", LOCAL);
 
     let mut clients: HashMap<String, Client> = HashMap::new();
+    let mut messages: Vec<String> = Vec::new();
+    let mut count: u8 = 0;
 
     let (tx, rx) = mpsc::channel::<String>();
     loop {
@@ -58,7 +63,7 @@ fn main() {
 
                         // Log the message and the sender
                         println!("{}", msg);
-                        tx.send(msg).expect("failed to send msg to rx");
+                        tx.send(msg).expect("Failed to send msg to rx");
                     }
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                     Err(_) => {
@@ -79,8 +84,40 @@ fn main() {
                     client.1.stream.write_all(&buff).map(|_| client).ok()
                 })
                 .collect::<HashMap<_, _>>();
+            messages.push(msg);
         }
 
+        if count >= 100 {
+            if Path::new(LOGS_PATH).exists() {
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open(Path::new(LOGS_PATH))
+                    .expect(format!("Couldn't open {}", LOGS_PATH).as_str());
+
+                for msg in messages {
+                    if let Err(e) = writeln!(file, "{}", msg) {
+                        eprintln!("Couldn't write to file: {}", e);
+                    }
+                }
+            } else {
+                // Open a file in write-only mode, returns `io::Result<File>`
+                let mut file = match File::create(Path::new(LOGS_PATH)) {
+                    Err(why) => panic!("Couldn't create {}: {}", LOGS_PATH, why),
+                    Ok(file) => file,
+                };
+                // Write the pyramid string to `file`, returns `io::Result<()>`
+                for msg in messages {
+                    if let Err(e) = writeln!(file, "{}", msg) {
+                        eprintln!("Couldn't write to: {}", e);
+                    }
+                }
+            }
+            count = 0;
+            messages = Vec::new();
+        }
+
+        count += 1;
         sleep();
     }
 }
